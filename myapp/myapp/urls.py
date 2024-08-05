df5526f99b92e8c781c7f8d305ae3634cc6d813e
salamaaya <105381544+salamaaya@users.noreply.github.com>
2024-08-05 13:29:32 -0400

    Add files via upload

---
+++ myapp/myapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login', views.user_login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('logout', views.user_logout, name='logout'),
+    path('increase/', views.increase, name='increase'),
+    path('decrease/', views.decrease, name='decrease'),
+    path('prev_month/', views.prev_month, name='prev_month'),
+    path('next_month/', views.next_month, name='next_month'),
+    path('get_date/', views.get_date, name='get_date'),
+    path('get_hijri/', views.get_hijri, name='get_hijri'),
+    path('reset_cal/', views.reset_cal, name='reset_cal'),
+]
