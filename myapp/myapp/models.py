305ce9f68aea7d0566a4275d58f200b5275f5cb1
salamaaya <105381544+salamaaya@users.noreply.github.com>
2024-08-05 13:29:00 -0400

    Delete myapp/myapp directory

--- myapp/myapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    completed_days = models.IntegerField(default=0)
-    remaining_days = models.IntegerField(default=0)
